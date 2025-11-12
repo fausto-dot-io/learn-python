c8be5980357a680f68bf814fd98b95b4c33b9dc5
Fausto Gonzalez <fgonzalez@tgh.org>
2025-11-12 14:23:44 -0500

    lfg

--- src/files/test_file_reading.py
+++ src/files/test_file_reading.py
@@ -36,9 +36,9 @@ def test_files_open():
     to file data is fine for text files, but will corrupt binary data like that in JPEG or EXE
     files. Be very careful to use binary mode when reading and writing such files.
 
-    It is good practice to use the with keyword when dealing with file objects. The advantage is
+    It is good practice to use the "with" keyword when dealing with file objects. The advantage is
     that the file is properly closed after its suite finishes, even if an exception is raised at
-    some point. Using with is also much shorter than writing equivalent try-finally blocks:
+    some point. Using "with" is also much shorter than writing equivalent try-finally blocks:
     """
 
     # Open files without using 'with' statement.
